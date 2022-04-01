bb9061a499817971d16b27b157354f7719733564
your_github_username <your_github_email>
2022-04-01 18:56:24 +0530

    first commit

---
+++ arrow.js
@@ -0,0 +1,16 @@
+//example1
+let myFunction = (a, b) => a * b;
+document.getElementById("demo").innerHTML = myFunction(4, 5);
+
+
+
+
+
+//example 2
+var hello;
+
+hello = () => {
+    return "Hello World!";
+}
+
+document.getElementById("demo").innerHTML = hello();
